d1c04625f03a3de2c43ff9b861836b54455ea955
adm <kdnkdn@list.ru>
2019-11-21 23:45:12 +0600

    yes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-System.out.println("Это текст на русском языке");
+System.out.println("Это просто текст");
     }
 }
