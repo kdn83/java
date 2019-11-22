f141a756685633969821abc4b91a9062d3f72856
adm <kdnkdn@list.ru>
2019-11-22 22:34:53 +0600

    yes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-System.out.println("Это просто текст");
+
+        int[] myArray; // объявление массива
+        myArray = new int[10]; // создание, то есть, выделение памяти для массива на 10 элементов типа int
+
+        int[] OrderArray; // объявление массива
+        OrderArray = new int[10]; // создание, то есть, выделение памяти для массива на 10 элементов типа int
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < 4; i++) {
+            int a = i+1;
+            System.out.println("ВВедите "+ a);
+            myArray[i] = scanner.nextInt();
+        }
+
+
+
+
+
+        for (int i = 0; i < 4; i++) {
+            System.out.print(myArray[i] + " " );
+        }
     }
 }
